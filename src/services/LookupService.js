430cb5c00aea1fced5e5668fa34806366c17606e
vincecarollo <vincecarollo@gmail.com>
2020-09-12 00:14:54 -0600

    Add lookup service to find company info

---
+++ src/services/LookupService.js
@@ -0,0 +1,18 @@
+const got = require('got')
+
+class LookupService {
+  static async companyLookup(searchTerm) {
+    const url = `https://reqres.in/api/unknown/${Math.floor(Math.random() * 11)}`
+    const response = await got(url)
+    if (response.statusCode != 200) { throw 'LookupFailed' }
+    const body = JSON.parse(response.body)
+    return {
+      website: body.ad.url,
+      phoneNumber: body.data.color,
+      email: body.data.pantone_value,
+      address: body.ad.company
+    }
+  }
+}
+
+module.exports = LookupService
